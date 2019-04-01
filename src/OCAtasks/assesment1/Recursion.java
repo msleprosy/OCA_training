af9bdaf156bfb3e112287ab84f23bec4232f7a63
msleprosy <msleprosy@yandex.ru>
2019-04-01 14:13:21 +0300

    training for OCA

--- src/OCAtasks/assesment1/Recursion.java
+++ src/OCAtasks/assesment1/Recursion.java
@@ -4,4 +4,16 @@ package OCAtasks.assesment1;
  * Created by veronika on 29.03.2019.
  */
 public class Recursion {
+    public static void main(String[] args) {
+        recur(99);
+    }
+
+    public static void recur(int a) {
+        if (a <= 100) {
+            System.out.println("a=" + a);
+            recur(++a);
+            System.out.println("a=" + a);
+        }
+    }
 }
+
