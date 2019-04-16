dd055523fb5927c9696c499154ba08f7b71bcfa0
msleprosy <msleprosy@yandex.ru>
2019-04-16 15:22:23 +0300

    updated

---
+++ src/OCAtasks/assesment1/arrays/ThreeDimensionalArray.java
@@ -0,0 +1,7 @@
+package OCAtasks.assesment1.arrays;
+
+/**
+ * Created by veronika on 16.04.2019.
+ */
+public class ThreeDimensionalArray {
+}
---
+++ src/OCAtasks/assesment1/arrays/TwoDimensionalToOneDimensionalArray.java
@@ -0,0 +1,7 @@
+package OCAtasks.assesment1.arrays;
+
+/**
+ * Created by veronika on 15.04.2019.
+ */
+public class TwoDimensionalToOneDimensionalArray {
+}
